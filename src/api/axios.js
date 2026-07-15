fe6ee5d8f633c96dfa115ad3096ef677d0dd7930
Mohamed <mohamedhadjkacem4@gmail.com>
2026-07-15 17:21:54 +0200

    Update project name

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+// environment variables need to start with VITE_ 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api',
 });
